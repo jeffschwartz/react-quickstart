4cad9a84a6a9dfe3fe3084af1845110834798049
Jeff Schwartz <jefftschwartz@gmail.com>
2016-09-10 11:01:22 -0400

    Now using statelss pure functions.

--- src/client/app/components/info.jsx
+++ src/client/app/components/info.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 
-export default class Info extends React.Component {
-    render () {
-        let {webpack, babel, eslint, react, reactbootstrap, rctg, redux, reduxform, reduxthunk, axios} = this.props;
-        return (
-            <div>
-                <h2>Includes:</h2>
-                <ul>
-                    <li>{react}</li>
-                    <li>{reactbootstrap}</li>
-                    <li>{rctg}</li>
-                    <li>{redux}</li>
-                    <li>{reduxform}</li>
-                    <li>{reduxthunk}</li>
-                    <li>{webpack}</li>
-                    <li>{babel}</li>
-                    <li>{eslint}</li>
-                    <li>{axios}</li>
-                </ul>
-            </div>
-        );
-    }
-}
+const Info = ({webpack, babel, eslint, react, reactbootstrap, rctg, redux, reduxform, reduxthunk, axios}) => (
+    <div>
+        <h2>Includes: </h2>
+        <ul>
+            <li>{react}</li>
+            <li>{reactbootstrap}</li>
+            <li>{rctg}</li>
+            <li>{redux}</li>
+            <li>{reduxform}</li>
+            <li>{reduxthunk}</li>
+            <li>{webpack}</li>
+            <li>{babel}</li>
+            <li>{eslint}</li>
+            <li>{axios}</li>
+        </ul>
+    </div>
+);
+
+export default Info;
